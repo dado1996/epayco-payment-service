8a994403d80a4b3a211c90aa01e33588fc67a806
Diego Delgado <diego.960705@gmail.com>
2025-02-01 03:56:48 -0500

    comment index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express, { json } from "express";
 import router from "./routes/routes.js";
 
-const PORT = process.env.APP_PORT || 3000;
-const app = express();
-app.use(json());
+const PORT = process.env.APP_PORT || 3000; // Get the port from the environment variable
+const app = express(); // Initialize the app
+app.use(json({ limit: "25kb" })); // The payload can't be bigger than 25 kb
 
-app.use("/api", router);
+app.use("/api", router); // Added "/api" prefix as it is standard for all APIs
 
 app.listen(PORT || 3000, () => {
   console.log("The epayco payment service is running at port " + PORT || 3000);
