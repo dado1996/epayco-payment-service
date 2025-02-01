06d516fb7bc7187125abb81d1a6fa58945a63489
Diego Delgado <diego.960705@gmail.com>
2025-01-31 22:30:04 -0500

    add api sufix at url as per standarization

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express, { json } from "express";
-import router from "./routes.js";
+import router from "./routes/routes.js";
 
 const PORT = process.env.APP_PORT || 3000;
 const app = express();
 app.use(json());
 
-app.use(router);
+app.use("/api", router);
 
 app.listen(PORT || 3000, () => {
   console.log("The epayco payment service is running at port " + PORT || 3000);
