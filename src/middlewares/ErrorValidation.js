e48793dcd6c119fa05a921155121129d7437c9e3
Diego Delgado <diego.960705@gmail.com>
2025-02-01 03:58:44 -0500

    minor adjustment in error capture

--- src/middlewares/ErrorValidation.js
+++ src/middlewares/ErrorValidation.js
@@ -1,8 +1,7 @@
 export const errorValidation = (err, request, response, next) => {
   console.error(err.stack);
-  response.status(500).json({
+  return response.status(500).json({
     status: 500,
     message: "An error has occurred: " + err.message,
   });
-  return;
 };
