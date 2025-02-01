ee16968bb083528bdb83524bf0923bea5d527ae8
Diego Delgado <diego.960705@gmail.com>
2025-02-01 05:04:58 -0500

    added transaction routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,23 @@
 import { Router } from "express";
 import UserRoutes from "./UserRoutes.js";
 import WalletRoutes from "./WalletRoutes.js";
+import TransactionRoutes from "./TransactionRoutes.js";
+import { NotFound } from "../middlewares/NotFound.js";
 import { errorValidation } from "../middlewares/ErrorValidation.js";
 
 const router = Router();
 
-router.get("/", (request, response) => {
-  return response.json({
-    status: 200,
-    message: "It works!",
-  });
-});
+// router.get("/", (request, response) => {
+//   return response.json({
+//     status: 200,
+//     message: "It works!",
+//   });
+// });
 
 router.use("/users", UserRoutes);
 router.use("/wallets", WalletRoutes);
+router.use("/transactions", TransactionRoutes);
+router.use(NotFound);
 
 router.use(errorValidation);
 
