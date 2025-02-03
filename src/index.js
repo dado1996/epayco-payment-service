0fc783d751c9318cf28404acb78fe5f274437a41
Diego Delgado <diego.960705@gmail.com>
2025-02-03 11:37:43 +0100

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express, { json } from "express";
 import router from "./routes/routes.js";
+import cors from "cors";
 
 const PORT = process.env.APP_PORT || 3000; // Get the port from the environment variable
 const app = express(); // Initialize the app
 app.use(json({ limit: "25kb" })); // The payload can't be bigger than 25 kb
+app.use(
+  cors({
+    exposedHeaders: ["GET, POST"],
+    origin: process.env.CLIENT_SERVICE_URL || "http://localhost:3001/",
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use("/api", router); // Added "/api" prefix as it is standard for all APIs
 
